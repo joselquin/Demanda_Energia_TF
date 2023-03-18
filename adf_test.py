41c90c5ff84d796773ff6c0aa385d7a0b6c92992
joselquin <joselquin@gmail.com>
2023-03-18 19:45:01 +0000

    Añade función para Dickey-Fuller test

---
+++ adf_test.py
@@ -0,0 +1,25 @@
+# Función para Augmented Dickey-Fuller test
+# Este test detecta si la serie temporal es o no estacionaria
+# (c) Pierian Data  - www.pieriandata.com
+
+from statsmodels.tsa.stattools import adfuller
+
+def adf_test(series):
+    
+    print(f'Augmented Dickey-Fuller Test')
+    result = adfuller(series.dropna(),autolag='AIC')
+    
+    labels = ['ADF test','p-value','# lags','# observaciones']
+    out = pd.Series(result[0:4],index=labels)
+
+    for key,val in result[4].items():
+        out[f'critical value ({key})']=val
+        
+    print(out.to_string())
+    
+    if result[1] <= 0.05:
+        print("Rechazamos la hipótesis nula")
+        print("La serie es estacionaria")
+    else:
+        print("No se puede rechazar la hipótesis nula")
+        print("La serie es no estacionaria")
